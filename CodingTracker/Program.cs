f5bc95d203be3d3fba131f8660071251220e35fa
Rodolfo Dornelas Passos de Oliveira <85281528+rushofred@users.noreply.github.com>
2025-12-11 17:56:51 -0300

    Keeping in one message

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,9 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C# Academy! One more Modification!");
-Console.WriteLine("Hello, C# Academy! One more Modification!");
-Console.WriteLine("Hello, C# Academy! One more Modification!");
-Console.WriteLine("Hello, C# Academy! One more Modification!");
-Console.WriteLine("Hello, C# Academy! One more Modification!");
-Console.WriteLine("Hello, C# Academy! One more Modification!");
-Console.WriteLine("Hello, C# Academy! One more Modification!");
 
